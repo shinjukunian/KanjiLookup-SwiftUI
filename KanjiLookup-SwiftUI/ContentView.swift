5480721c23237439a96fa7226fc1fe9b71f4c1da
Morten Bertz <morten@telethon.jp>
2020-06-17 14:13:34 +0900

    Initial Commit

---
+++ KanjiLookup-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KanjiLookup-SwiftUI
+//
+//  Created by Morten Bertz on 2020/06/17.
+//  Copyright © 2020 telethon k.k. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
